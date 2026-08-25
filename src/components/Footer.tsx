85698c87dab937b4e53a57510f75bfc97dbc6770
viniciusofp <viniciusofp@gmail.com>
2026-08-25 13:47:38 -0300

    less shadow and footer padding

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ export type FooterProps = {};
 
 export default function Footer(props: FooterProps) {
   return (
-    <div className="relative z-1 w-full pt-24 pb-6">
+    <div className="relative z-1 w-full pt-12 pb-6">
       <footer className="relative z-2 mx-auto w-fit rounded-xs bg-white/40 px-3 py-1.5 text-center font-mono text-[10px] tracking-widest uppercase backdrop-blur-xs">
         Desenvolvido por{" "}
         <Link
